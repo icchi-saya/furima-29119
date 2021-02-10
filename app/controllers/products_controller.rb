419c11b46813980f8e91d049d1f7125ddf7c10a8
icchi-saya <71650432+icchi-saya@users.noreply.github.com>
2021-02-10 21:01:42 +0900

    コードレビュー変更１

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_product, only: [:show, :edit, :update]
 
   def index
     @products = Product.all.order("created_at DESC")
@@ -19,15 +20,12 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def edit
-    @product = Product.find(params[:id])
   end
 
   def update
-    @product = Product.find(params[:id])
     if @product.update(products_params)
       redirect_to root_path
     else 
@@ -39,6 +37,10 @@ class ProductsController < ApplicationController
 
   def products_params
     params.require(:product).permit(:image, :product_name, :description, :category_id, :status_id, :shipping_charge_id, :shipping_area_id, :days_ship_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 
 end
