fd293613033bb6d69c40d4a6097c35795482d7fc
icchi-saya <71650432+icchi-saya@users.noreply.github.com>
2021-02-11 19:01:25 +0900

    商品削除機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -33,6 +33,12 @@ class ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to root_path
+  end
+
   private
 
   def products_params
