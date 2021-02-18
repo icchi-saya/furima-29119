55b71d11b4f47ec8a8a4c957429c5d4e4efa074a
icchi-saya <71650432+icchi-saya@users.noreply.github.com>
2021-02-18 17:34:41 +0900

    Revert "直打ち防止設定など"
    This reverts commit 896cdf13d76825093ba0d567840c58bd0d4c0fac.

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,11 +1,6 @@
 class PurchaseRecordsController < ApplicationController
-  before_action :authenticate_user!
-  before_action :set_product, only: [:index, :create]
-  before_action :buyer_only, only: [:index]
-  before_action :sold_out, only: [:index]
-  
-
-  def index
+    def index
+    @product = Product.find(params[:product_id])
     @purchase_user = PurchaseUser.new
   end
 
@@ -14,6 +9,7 @@ class PurchaseRecordsController < ApplicationController
   end
   
   def create
+    @product = Product.find(params[:product_id])
     @purchase_user = PurchaseUser.new(purchase_params)
     if @purchase_user.valid?
       pay_product
@@ -25,10 +21,6 @@ class PurchaseRecordsController < ApplicationController
   end
 
   private
-
-  def set_product
-    @product = Product.find(params[:product_id])
-  end
 
   def purchase_params
     params.require(:purchase_user).permit(:postal_code, :prefectures_id, :city, :house_number, :apartment, :phone_number).merge(product_id: params[:product_id],user_id: current_user.id,token: params[:token])
@@ -43,13 +35,6 @@ class PurchaseRecordsController < ApplicationController
     )
   end
 
-  def buyer_only
-    return redirect_to root_path if current_user.id == @product.user.id  
-  end
-
-  def sold_out
-    return redirect_to root_path if @product.purchase_record
-  end
 end
 
 
